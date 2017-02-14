68c0a9a8a723a51ad9cb833b7da39967cd73375f
David Konsumer <konsumer@jetboystudio.com>
2017-02-14 15:49:37 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const grpc = require('grpc')
+const resolve = require('path').resolve
+
+const server = new grpc.Server()
+const proto = grpc.load({
+  file: resolve(__dirname, 'proto/demo/rpc/helloworld.proto'),
+  root: resolve(__dirname, 'proto')
+})
+
+server.addProtoService(proto.demo.Greeter.service, {
+  sayHello: (ctx, cb) => {
+    const message = `Hello ${ctx.request.name}.`
+    cb({message})
+  }
+})
+
+server.bind('localhost:50051', grpc.credentials.createInsecure())
